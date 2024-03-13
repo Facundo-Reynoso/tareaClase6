ad5ea68a2d476749a49cb2189de0937a49d2fac4
Facundo Reynoso <reynosofacundo1@hotmail.com>
2024-03-13 05:18:04 -0300

    añade funcion
    esta función crea el formulario 2

--- tarea1/index.js
+++ tarea1/index.js
@@ -1,2 +1,24 @@
 const btnGenerarFormularios = document.querySelector('#btn-generar-formularios')
 
+btnGenerarFormularios.onclick = function(){
+    let cantidadDeMiembros = Number(document.querySelector('#cantidad-de-miembros').value)
+    let formulario2 = document.querySelector('#formulario-2')
+
+    formulario2.innerHTML = `<h3>Edad de los miembros:</h3>`
+
+    for(i = 1; i <= cantidadDeMiembros; i++){
+        formulario2.innerHTML += `
+            <br>
+            <label for="edad-miembro-${i}">Ingrese la edad del miembro ${i}</label>
+            <input type="number" min="0" id="edad-miembro-${i}"
+            <br>
+        `
+    }
+
+    formulario2.innerHTML += `
+        <br>
+        <button id="btn-calcular">Calcular</button>
+    `
+    return false
+}
+
