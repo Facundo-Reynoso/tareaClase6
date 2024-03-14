aa978a8e62c1884d649b0cc49b57ae5cd1ffd10c
Facundo Reynoso <reynosofacundo1@hotmail.com>
2024-03-14 05:48:12 -0300

    vinculo el botón calcular a una variable

--- tarea2/index.js
+++ tarea2/index.js
@@ -2,6 +2,8 @@ const btnAgregar = document.querySelector('#btn-agregar')
 const btnQuitar = document.querySelector('#btn-quitar')
 let cantidadDeMiembros = 0
 
+const btnCalcular = document.querySelector('#btn-calcular')
+
 btnAgregar.onclick = function(){
     cantidadDeMiembros++
 
