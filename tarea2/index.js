9613848aae93b83e83a8647a5f88226031e4733a
Facundo Reynoso <reynosofacundo1@hotmail.com>
2024-03-14 05:00:44 -0300

    agrega funcion del botn agregar
    empieza por sumar el miembro

--- tarea2/index.js
+++ tarea2/index.js
@@ -2,3 +2,8 @@ const btnAgregar = document.querySelector('#btn-agregar')
 const btnQuitar = document.querySelector('#btn-quitar')
 let cantidadDeMiembros = 0
 
+btnAgregar.onclick = function(){
+    cantidadDeMiembros++
+
+
+}
